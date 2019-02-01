6b7794fad17fbdb1a411ca701abc410aa65f9ff3
Eirik Myrvoll-Nilsen <eirikmn91@gmail.com>
2019-02-01 14:54:23 +0100

    Updated H.map.maker.R to allow for the case of multiple R libraries
    Hopefully this brings Windows support

--- R/h.map.maker.R
+++ R/h.map.maker.R
@@ -4,15 +4,33 @@ h.map.maker = function(m=4,lagmax=1000,stoc="fgn"){
     stop(paste0("'",stoc,"' is not supported."))
   }
   
-  if(length(list.files(path=file.path(.Library,"/INLA.climate/Hmapping/",fsep="")))==0)
-    stop(paste0("Directory ",file.path(.Library,"/INLA.climate/Hmapping/",fsep="")," is empty. Please make sure that INLA.climate package is properly installed."))
+#  load(file=system.file("inst/Hmapping/",package="INLA.climate"))
+  
+  folder.path=c()
+  for(i in 1:length(.libPaths())){
+    if("INLA.climate" %in% list.files(.libPaths()[i])){
+      folder.path=paste0(.libPaths()[i],"/INLA.climate/Hmapping/")
+    }
+  }
+  if(length(folderpath)==0){
+    stop("Could not find package directory, please make sure that INLA.climate is installed within one of the libraries displayed by '.libPaths()'.")
+  }
+  
+
+  
+  #if(length(list.files(path=file.path(.Library,"/INLA.climate/Hmapping/",fsep="")))==0)
+  if(length(list.files(path=folder.path))==0){
+    stop(paste0("Directory ",folder.path," is empty. Please make sure that INLA.climate package is properly installed."))
+    
+  }
+    
   
   filename = paste(stoc,"Lag",lagmax,"m",m,".txt",sep="") 
   
-  if(!( filename %in% list.files(path=file.path(.Library,"/INLA.climate/Hmapping/",fsep="")))){
+  if(!( filename %in% list.files(path=folder.path))){
     stop("Model specifications are not supported.")
   }
-  filepath = file.path(.Library,"/INLA.climate/Hmapping/",filename,fsep="")
+  filepath = file.path(folder.path,filename,fsep="")
   
   data = read.table(filepath)
   

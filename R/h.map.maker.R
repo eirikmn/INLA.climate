d23d25f0c18745dec74baab29724a483de1dea3b
eirikmn <eirikmn91@gmail.com>
2023-01-27 10:34:13 +0100

    Fixed bug in h.map.maker

--- R/h.map.maker.R
+++ R/h.map.maker.R
@@ -1,39 +1,39 @@
 h.map.maker = function(m=4,lagmax=1000,model="fgn"){
-  
+
   if(!(model %in% c("fgn","arfima"))){
     stop(paste0("'",model,"' is not supported."))
   }
-  
+
 #  load(file=system.file("inst/Hmapping/",package="INLA.climate"))
-  
+
   folder.path=c()
   for(i in 1:length(.libPaths())){
-    if("INLA.climate2" %in% list.files(.libPaths()[i])){
-      folder.path=paste0(.libPaths()[i],"/INLA.climate2/Hmapping/")
+    if("INLA.climate" %in% list.files(.libPaths()[i])){
+      folder.path=paste0(.libPaths()[i],"/INLA.climate/Hmapping/")
     }
   }
   if(length(folder.path)==0){
     stop("Could not find package directory, please make sure that INLA.climate is installed within one of the libraries displayed by '.libPaths()'.")
   }
-  
 
-  
+
+
   #if(length(list.files(path=file.path(.Library,"/INLA.climate/Hmapping/",fsep="")))==0)
   if(length(list.files(path=folder.path))==0){
     stop(paste0("Directory ",folder.path," is empty. Please make sure that INLA.climate package is properly installed."))
-    
+
   }
-    
-  
-  filename = paste(model,"Lag",lagmax,"m",m,".txt",sep="") 
-  
+
+
+  filename = paste(model,"Lag",lagmax,"m",m,".txt",sep="")
+
   if(!( filename %in% list.files(path=folder.path))){
     stop("Model specifications are not supported.")
   }
   filepath = file.path(folder.path,filename,fsep="")
-  
+
   coofdata = read.table(filepath)
-  
+
   warn.old = getOption("warn") ## grid spacing in coofdata is too narrow for splinefun to distinguish
   options(warn=-1)              ## them properly. It produces a warning which is here suppressed.
   funks = numeric(0)
@@ -42,7 +42,7 @@ h.map.maker = function(m=4,lagmax=1000,model="fgn"){
     funks = c(funks,fun)
   }
   options(warn=warn.old)
-  
+
   return(funks)
 }
 
